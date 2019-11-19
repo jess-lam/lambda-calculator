a6b6e9efb3a473134b268294a8b20d12876551b5
Jessica Lam <jemlam@Jessicas-MacBook-Pro.local>
2019-11-19 14:31:46 -0800

    deleted {} from imports

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { OperatorButton } from "./OperatorButton/"
-import { operators } from "../../../data";
+import OperatorButton from "./OperatorButton"
+import operators from "../../../data";
 //import any components needed
 
 //Import your array data to from the provided data file
@@ -13,9 +13,7 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       operatorState.map((operatorObject, index) => 
-       <OperatorButton key={index} char={operatorObject.char} value={operatorObject.value}/>
-        )
+       console.log(operatorState);
     </div>
   );
 };
