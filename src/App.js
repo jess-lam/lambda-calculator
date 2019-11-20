a62804fae0c81ad11a872b2ce7a1f589f12d7ae9
Jessica Lam <jemlam@jessicas-mbp.lan>
2019-11-19 17:50:08 -0800

    added display

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Display from "./components/DisplayComponents/Display";
 
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
@@ -20,6 +21,7 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <Display />
       <Numbers />
       <Operators />
       <Specials />
